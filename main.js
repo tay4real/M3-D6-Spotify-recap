5c26e44197d2f6f60e10126ca63c53d5232bb4e3
Stefano Miceli <mic@nucleode.com>
2020-11-09 09:12:09 +0100

    Adding M3-D6 Spotify files

---
+++ main.js
@@ -0,0 +1,142 @@
+let rockArtists = [
+  "queen",
+  "u2",
+  "thepolice",
+  "eagles",
+  "thedoors",
+  "oasis",
+  "thewho",
+  "bonjovi",
+];
+
+let popArtists = [
+  "maroon5",
+  "coldplay",
+  "onerepublic",
+  "jamesblunt",
+  "katyperry",
+  "arianagrande",
+];
+
+let hipHopArtists = ["eminem", "snoopdogg", "lilwayne", "drake", "kanyewest"];
+
+const albumCard = (songInfo) => {
+  return `
+          <div class="col text-center" id=${songInfo.id}>
+            <a href="/album_page.html?id=${songInfo.album.id}"> <img class="img-fluid" src=${songInfo.album.cover_medium} alt="img of ${songInfo.artist.name}"/></a>
+              <p> <a href="/album_page.html?id=${songInfo.album.id}">Album: "${songInfo.album.title}"</a>
+              <br>
+              <a href="/artist_page.html?artistId=${songInfo.artist.id}">Artist: "${songInfo.artist.name}"</a>
+              </p>
+          </div>
+        `;
+};
+
+let query = "metallica";
+
+const handleSearchQuery = (e) => {
+  query = e.target.value;
+};
+
+const headers = new Headers({
+  "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+  "x-rapidapi-key": "4013e328ffmsh3feb54311ce7296p1c3cc4jsnd3ad09e0821d",
+});
+
+const search = (q = query, firstLoad = false) => {
+  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + q, {
+    method: "GET",
+    headers,
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((songs) => {
+      const songsInfo = songs.data;
+      document.querySelector("#results > h2").innerText = query;
+      const row = document.querySelector("#results > .row");
+
+      row.innerHTML = "";
+
+      songsInfo.forEach((song) => {
+        const col = document.createElement("div");
+        col.className = "col";
+        col.id = song.id;
+        col.innerHTML += albumCard(song);
+        col.onclick = (e) => setPlayerSong(song);
+        row.appendChild(col);
+      });
+
+      if (firstLoad) {
+        const storageSongId = window.localStorage.getItem("songId");
+        const prevSong = songsInfo.find(
+          (song) => song.id === parseInt(storageSongId)
+        );
+        if (storageSongId && prevSong) {
+          if (prevSong) {
+            setPlayerInfos(prevSong);
+          }
+        } else {
+          setPlayerInfos(songsInfo[0]);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getSongsFromArr = (arr, secTitle) => {
+  let arrOfPromises = arr.map((artist) =>
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + artist, {
+      method: "GET",
+      headers,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .catch((err) => console.log(err))
+  );
+  document.querySelector("#results > h2").innerText = secTitle;
+  const row = document.querySelector("#results > .row");
+
+  row.innerHTML = `<div class="pl-3">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>`;
+
+  let reducer = (acc, current) => acc.concat(...current.data.slice(0, 4));
+
+  Promise.all(arrOfPromises).then((resp) => {
+    row.innerHTML = "";
+
+    resp
+      .map((arr) => arr)
+      .reduce(reducer, [])
+      .forEach((song) => {
+        const col = document.createElement("div");
+        col.className = "col";
+        col.id = song.id;
+        col.innerHTML += albumCard(song);
+        col.onclick = () => setPlayerSong(song);
+        row.appendChild(col);
+      });
+  });
+};
+
+window.onload = function () {
+  search(query, true);
+
+  //load handles on menu items
+  document.getElementById("Rock").onclick = () =>
+    getSongsFromArr(rockArtists, "Rock");
+  document.getElementById("Pop").onclick = () =>
+    getSongsFromArr(popArtists, "Pop");
+  document.getElementById("Hip Hop").onclick = () =>
+    getSongsFromArr(hipHopArtists, "Hip Hop");
+};
---
+++ player.js
@@ -0,0 +1,141 @@
+let audio = document.getElementsByTagName("audio")[0];
+let playIcon = document.querySelector("img[src$='Play.png']");
+let mute = false;
+let prevVolume = 1;
+let volumeIconClass;
+let range;
+
+window.addEventListener("DOMContentLoaded", () => {
+  range = document.querySelector(".playerVolume input[type='range']");
+
+  volumeIconClass =
+    range.value < 10
+      ? "off"
+      : range.value < 30
+      ? "down"
+      : range.value > 30
+      ? "up"
+      : "";
+
+  //localStorage
+  let storageVolume = window.localStorage.getItem("prevVolume");
+  if (storageVolume) {
+    prevVolume = storageVolume;
+    audio.volume = storageVolume;
+    range.setAttribute("value", storageVolume * 100);
+    changeVolIcon(storageVolume * 100);
+  }
+});
+
+const setPlayerSong = (song) => {
+  audio.src = song.preview;
+  playIcon.src = "playerbuttons/Pause.png";
+  setPlayerInfos(song);
+  window.localStorage.setItem("songId", song.id);
+  audio.play();
+};
+
+const setPlayerInfos = (song) => {
+  let playerArtistInfo = document.querySelector(".playerArtistInfo");
+  let img = playerArtistInfo.querySelector("img");
+  let songTitle = playerArtistInfo.querySelector("h6");
+  let songAlbum = playerArtistInfo.querySelector("p");
+  img.src = song.album.cover;
+  img.classList.add("img-fluid");
+  img.style.cssText = "height: auto; max-height: 50px";
+  songTitle.innerText = song.title;
+  songAlbum.innerText = song.album.title;
+  audio.src = song.preview;
+
+  let player = document.querySelector(".player");
+  let duration = player.querySelector(".duration");
+  let currTime = player.querySelector(".currentTime");
+  currTime.innerText = "00:00";
+  duration.innerText =
+    Math.floor(song.duration / 60) + ":" + Math.floor(song.duration % 60);
+};
+
+const changeProgressBar = (node, perc) => {
+  node.style.width = perc.toString() + "%";
+  node.setAttribute("aria-valuenow", perc.toString());
+};
+
+const handleProgressBarUpdate = (ended = false) => {
+  let elem = document.querySelector(".progress-bar");
+  let percentage = (audio.currentTime / audio.duration) * 100;
+
+  changeProgressBar(elem, percentage);
+
+  if (ended) {
+    changeProgressBar(elem, 0);
+    playIcon.src = "playerbuttons/Play.png";
+  }
+  let player = document.querySelector(".player");
+  let currTime = player.querySelector(".currentTime");
+  let minutes = "0" + Math.floor(audio.currentTime / 60);
+  let seconds = "0" + Math.floor(audio.currentTime);
+  currTime.innerText = minutes.substr(-2) + ":" + seconds.substr(-2);
+};
+
+const handleProgressBarClick = (e) => {
+  let elem = e.currentTarget.firstElementChild;
+  let percentage = parseInt((e.offsetX * 100) / e.currentTarget.clientWidth);
+  elem.style.width = percentage.toString() + "%";
+  elem.setAttribute("aria-valuenow", percentage.toString());
+
+  audio.currentTime = (percentage / 100) * audio.duration;
+};
+
+const changeVolIcon = (value) => {
+  if (value < 10) {
+    document.querySelector(".playerVolume i").className = "fa fa-volume-off";
+    volumeIconClass = "off";
+  } else if (value > 10 && value < 30) {
+    document.querySelector(".playerVolume i").className = "fa fa-volume-down";
+    volumeIconClass = "down";
+  } else if (value > 30) {
+    document.querySelector(".playerVolume i").className = "fa fa-volume-up";
+    volumeIconClass = "up";
+  }
+};
+
+const handleChangeVolume = (e) => {
+  let volume = e.target.value / 100;
+  prevVolume = volume;
+
+  window.localStorage.setItem("prevVolume", volume);
+  audio.volume = volume;
+
+  changeVolIcon(e.target.value);
+};
+
+const handleMute = () => {
+  if (mute) {
+    document.querySelector(".playerVolume i").className =
+      "fa fa-volume-" + volumeIconClass;
+    mute = false;
+    audio.volume = prevVolume;
+  } else {
+    document.querySelector(".playerVolume i").className = "fa fa-volume-mute";
+    mute = true;
+    audio.volume = 0;
+  }
+};
+
+const handlePlay = () => {
+  if (audio.played.length > 0) {
+    if (audio.paused) {
+      playIcon.src = "playerbuttons/Pause.png";
+      audio.play();
+    } else {
+      playIcon.src = "playerbuttons/Play.png";
+      audio.pause();
+    }
+  } else {
+    playIcon.src = "playerbuttons/Pause.png";
+    audio.play();
+  }
+};
+
+audio.ontimeupdate = () => handleProgressBarUpdate();
+audio.onended = () => handleProgressBarUpdate(true);
